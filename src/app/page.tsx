ac2d40daa81d078eb036e5e7d5826b9baf507051
VHall1 <victorhallpsn@hotmail.com>
2023-08-24 16:23:11 +0100

    query and render notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main>hello</main>;
+import { prisma } from "@/util/prisma";
+
+function getNotes() {
+  return prisma.note.findMany();
+}
+
+export default async function Home() {
+  const notes = await getNotes();
+
+  return (
+    <main>
+      <pre>{JSON.stringify(notes, undefined, 2)}</pre>
+    </main>
+  );
 }
